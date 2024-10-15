040e5ec77331a0e5396622a7d57199b1d244fde6
felix maati <maatifelix001@gmail.com>
2024-10-15 23:47:13 +0300

    add correct status codes

--- server/resources.py
+++ server/resources.py
@@ -10,8 +10,7 @@ class Episodes(Resource):
         """Get all episodes with their episode number and date."""
         episode_list = Episode.query.all()
         episode_dict_list = [
-            episode.to_dict(rules=('-appearances', ))
-            for episode in episode_list
+            episode.to_dict(rules=("-appearances",)) for episode in episode_list
         ]
         return episode_dict_list
 
@@ -25,14 +24,14 @@ class EpisodeByID(Resource):
 
         try:
             if not episode:
-                raise ValueError(f'Episode with ID {id} does not exist')
+                raise ValueError(f"Episode with ID {id} does not exist")
 
-            episode_dict = episode.to_dict(rules=('-appearances.episode', ))
+            episode_dict = episode.to_dict(rules=("-appearances.episode",))
 
             return episode_dict
         except ValueError as e:
             errors.append(str(e))
-            return {'errors': errors}
+            return {"errors": errors}, 400
 
 
 class Appearances(Resource):
@@ -46,21 +45,21 @@ class Appearances(Resource):
         errors = []
 
         try:
-            rating = data['rating']
-            episode_id = data['episode_id']
-            guest_id = data['guest_id']
+            rating = data["rating"]
+            episode_id = data["episode_id"]
+            guest_id = data["guest_id"]
 
             episode = db.session.get(Episode, episode_id)
             guest = db.session.get(Guest, guest_id)
 
             if not episode:
-                raise ValueError('Invalid episode_id')
+                raise ValueError("Invalid episode_id")
             if not guest:
-                raise ValueError('Invalid guest_id')
+                raise ValueError("Invalid guest_id")
 
-            new_appearance = Appearance(guest_id=guest_id,
-                                        episode_id=episode_id,
-                                        rating=rating)
+            new_appearance = Appearance(
+                guest_id=guest_id, episode_id=episode_id, rating=rating
+            )
             db.session.add(new_appearance)
             db.session.commit()
 
@@ -68,14 +67,13 @@ class Appearances(Resource):
 
             return new_appearance_dict, 201
         except KeyError as e:
-            errors.append(f'Missing required field: {e}')
+            errors.append(f"Missing required field: {e}")
         except ValueError as e:
             errors.append(str(e))
         except IntegrityError as e:
-            if 'UNIQUE' in str(e):
-                errors.append(
-                    f'Duplicate Appearance. Appearance already exists')
-        return {'errors': errors}
+            if "UNIQUE" in str(e):
+                errors.append(f"Duplicate Appearance. Appearance already exists")
+        return {"errors": errors}, 400
 
 
 class Guests(Resource):
@@ -85,7 +83,7 @@ class Guests(Resource):
         guest_list = Guest.query.all()
 
         guest_dict_list = [
-            guest.to_dict(rules=('-appearances', )) for guest in guest_list
+            guest.to_dict(rules=("-appearances",)) for guest in guest_list
         ]
 
         return guest_dict_list
